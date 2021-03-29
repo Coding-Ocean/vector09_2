c60eb2aaac12419671dea4509c9140c2492b7c72
Coding-Ocean <eijo524@gmail.com>
2021-03-29 22:49:27 +0900

    atan2

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,29 +1,36 @@
 #include"libOne.h"
-void gmain(){
+void gmain() {
     window(1000, 1000);
     angleMode(DEGREES);
-    float x, y, angle=0;
-    float tx, ty;
-    float tanAngle;
+    colorMode(HSV);
+    float satu = 64;
+    float x, y, angle = 0;
     while (notQuit) {
-        x = cos(angle);
-        y = sin(angle);
-        tanAngle = tan(angle);
-        tx = y * tanAngle;
-        ty = -x * tanAngle;
-        angle += 0.2f;
-        clear(128);
-        mathAxis(5.1f);
-        strokeWeight(4);
-        fill(0, 0, 0, 0);
-        mathCircle(0, 0, 2);
-        stroke(0);
+        clear(120, 255, 50);
+        mathAxis(5.1f, 255);
+
+        x = mathMouseX;
+        y = mathMouseY;
+        angle = atan2(y, x);
+
+        strokeWeight(10);
+        stroke(0, 0, 128);
         mathArrow(0, 0, x, y);
-        stroke(160, 200, 255);
-        mathLine(x, y, x + tx, y + ty);
+        stroke(240, satu, 255);
+        mathLine(0, 0, x, 0);
+        stroke(0, satu, 255);
+        mathLine(x, 0, x, y);
+
+        stroke(60, satu, 255);
+        mathArc(0, angle, 0.5f);
 
         textSize(50);
         fill(0);
-        text((let)"tanAngle="+tanAngle,0,50);
+        fill(240, satu, 255);
+        text((let)"x=" + x, 0, 50);
+        fill(0, satu, 255);
+        text((let)"y=" + y, 0, 100);
+        fill(60, satu, 255);
+        text((let)"atan2(y,x)=" + angle, 0, 150);
     }
 }
